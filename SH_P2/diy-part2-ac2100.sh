de6d8bde557bb38d949fe9991b7bf56a9b410c03
nazo <44923159+nazo-x1@users.noreply.github.com>
2022-07-14 15:15:58 +0800

    add rm-ac2100

---
+++ SH_P2/diy-part2-ac2100.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# Modify default SSID
+sed -i "s/DISTRIB_ID.*/DISTRIB_ID='rm-ac2100'/g" package/base-files/files/etc/openwrt_release
+
+# Modify hostname
+sed -i 's/OpenWrt/rm-ac2100/g' package/base-files/files/bin/config_generate
+
+# Modify the version number
+sed -i "s/OpenWrt /0NAZO0 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="0NAZO0"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1"0NAZO0"@' .config
+
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1"GitHub Actions"@' .config
+
+mkdir package/community
+pushd package/community
+
+#add filebrowser
+git clone --depth=1 https://github.com/immortalwrt/openwrt-filebrowser
+
+#add gowebdav
+git clone --depth=1 https://github.com/immortalwrt/openwrt-gowebdav
+
+# Add luci-app-vssr <M>
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
+
+# Add pushbot
+git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot
+
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+
+# Add OpenAppFilter
+git clone --depth=1 https://github.com/destan19/OpenAppFilter
+
+# Add fileAssistant
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant
+
+# Add smartdns
+git clone https://github.com/pymumu/smartdns.git
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns.git
+
+popd
+
+# del ori argon
+rm -rf package/lean/luci-theme-argon
+rm -rf package/feeds/luci/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
+
+# del ori pushbot
+rm -rf package/feeds/luci/luci-app-pushbot
+rm -rf feeds/luci/applications/luci-app-pushbot
+
+# del ori smartdns
+rm -rf feeds/packages/net/smartdns
+
